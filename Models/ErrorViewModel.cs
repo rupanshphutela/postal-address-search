6837971e3b4c29332b59a620dde4efa6393e7d6f
Jasmine dhunna <jasmine9003@gmail.com>
2022-03-03 20:26:54 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AddressManager_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Service/AddressDataService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressManager_final.Service
+{
+    public class AddressDataService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+        public AddressDataService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string GetAddressJsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Addresses.json"); }
+        }
+    }
+}
