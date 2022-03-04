a20ca18989674b10294ede0fcef088c4a21588b5
Jasmine dhunna <jasmine9003@gmail.com>
2022-03-03 22:09:02 -0800

    Added new file Address.cs and its properties.

---
+++ Models/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressManager_final.Models
+{
+    public class Address
+    {
+        public string buildingNumber { get; set; }
+        public string streetName { get; set; }
+        public string neighborhood { get; set; }
+        public string quadrant { get; set; }
+        public string city { get; set; }
+        public string postalCOde { get; set; }
+        public string stateOrProvince { get; set; }
+        public string country { get; set; }
+        public string prefectureName { get; set; }
+        public string subArea { get; set; }
+        public string furtherSubArea { get; set; }
+        public string block { get; set; }
+        public string subdivisionOrPrecinct { get; set; }
+
+    }
+}
