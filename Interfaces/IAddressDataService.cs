8544946c2a8a20ef0f6c1f68455eea5201eeb4b0
Jasmine dhunna <jasmine9003@gmail.com>
2022-03-05 02:22:26 -0800

    Created interface IAddressDataService.cs

---
+++ Interfaces/IAddressDataService.cs
@@ -0,0 +1,11 @@
+﻿using AddressManager_final.Models;
+using System.Collections.Generic;
+
+
+namespace AddressManager_final.Interfaces
+{
+    interface IAddressDataService
+    {
+        public IEnumerable<Address> GetAllAddresssesData();
+    }
+}
