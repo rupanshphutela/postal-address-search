f41aaea4daf274dc8e0ad8a4c05f95636a2dc20e
Jasmine dhunna <jasmine9003@gmail.com>
2022-03-05 02:25:35 -0800

    Exposed two end point api/Country and api/Countries in HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using AddressManager_final.Models;
+﻿using AddressManager_final.Interfaces;
+using AddressManager_final.Models;
+using AddressManager_final.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,18 +13,45 @@ namespace AddressManager_final.Controllers
 {
     public class HomeController : Controller
     {
+        
         private readonly ILogger<HomeController> _logger;
+        private readonly IAddressDataService _dataService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AddressDataService dataService)
         {
             _logger = logger;
+            _dataService = dataService;
+        }
+
+        [HttpGet("api/Countries")]
+        public List<string> GetCountries()
+        {
+            var addresses = _dataService.GetAllAddresssesData();
+            var countries = addresses.Select(c => c.country).Distinct();
+            return countries.ToList();
+        }
+
+        [HttpGet("api/Country/{countryName}")]
+        public ActionResult Country(string countryName)
+        {
+            var address = _dataService.GetAllAddresssesData().Where(c => c.country == countryName).FirstOrDefault();
+            var result = typeof(Address).GetProperties()
+                .Select(x => new { property = x.Name, value = x.GetValue(address) })
+                .Where(x => x.value != null)
+                .ToList().Select(r => r.property);
+            return View(result);
         }
 
         public IActionResult Index()
         {
-            return View();
+            var addresses = _dataService.GetAllAddresssesData();
+            var countries = addresses.Select(c => c.country).Distinct();
+            return View(addresses.FirstOrDefault());
         }
 
+
+
+        [HttpPost("test")]
         public IActionResult Privacy()
         {
             return View();
