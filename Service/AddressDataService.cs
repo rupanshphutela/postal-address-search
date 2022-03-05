0ee79d70b3853447e672b392def4f418628f83ce
Jasmine dhunna <jasmine9003@gmail.com>
2022-03-05 02:23:16 -0800

    Added method GetAllAddresssesData to AddressDataService.cs

--- Service/AddressDataService.cs
+++ Service/AddressDataService.cs
@@ -1,23 +1,38 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using AddressManager_final.Interfaces;
+using AddressManager_final.Models;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AddressManager_final.Service
 {
-    public class AddressDataService
+    public class AddressDataService: IAddressDataService
     {
-        public IWebHostEnvironment WebHostEnvironment { get; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
         public AddressDataService(IWebHostEnvironment webHostEnvironment)
         {
-            WebHostEnvironment = webHostEnvironment;
+            _webHostEnvironment = webHostEnvironment;
         }
 
-        private string GetAddressJsonFileName
+        private string AddressJsonFileName
         {
-            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "Addresses.json"); }
+            get { return Path.Combine(_webHostEnvironment.WebRootPath, "data", "Addresses.json"); }
+        }
+
+        public IEnumerable<Address> GetAllAddresssesData()
+        {
+            using (var jsonFileReader = File.OpenText(AddressJsonFileName))
+            {
+                return JsonSerializer.Deserialize<Address[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
         }
     }
 }
